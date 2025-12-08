8eadb9966da560b6a9764b79dab0578368533098
Abir Chowdhury <abirchowdhury0928@gmail.com>
2025-12-08 17:55:44 +0600

    Add some feather

---
+++ app/docs/[contentId]/[subContentId]/page.jsx
@@ -0,0 +1,12 @@
+import ContentDisplay from '@/components/ContentDisplay'
+import React from 'react'
+
+const SubContentPage = ({ params: { subContentId } }) => {
+    return (
+        <div>
+            <ContentDisplay id={subContentId} />
+        </div>
+    )
+}
+
+export default SubContentPage
